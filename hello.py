165536f6d55d954a8f656d635e4540c31d6e2bcb
xiaomacai <xmc1028@gmail.com>
2017-02-13 19:18:41 +0800

    finished database function

--- hello.py
+++ hello.py
@@ -1,36 +1,69 @@
 # coding:utf8
 from flask import Flask
 from flask import render_template, redirect, url_for, session, flash
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
-from flask_wtf import Form
+from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_migrate import MigrateCommand, Migrate
+import os
 
+basedir = os.path.abspath(os.path.dirname(__name__))
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = "SDF324SFW#*#fs4523sdfER@#5gfjj436.vm,65"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField(u'输入你的姓名:', validators=[DataRequired()])
     submit = SubmitField(u'确认')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.INTEGER, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.INTEGER, primary_key=True)
+    userName = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.INTEGER, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.userName
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash(u'名字已修改!')
+        user = User.query.filter_by(userName=form.name.data).first()
+        if user is None:
+            user = User(userName=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @app.route('/user/<username>')
@@ -46,6 +79,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internet_server_error(e):
     return render_template('500.html'), 500
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command('shell', Shell(make_context=make_shell_context))
 
 
 if __name__ == '__main__':
