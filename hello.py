7404462082dce9cfb40ab1d21a392466c96e560e
xiaomacai <xmc1028@gmail.com>
2017-02-13 22:55:27 +0800

    reset to form part

--- hello.py
+++ hello.py
@@ -63,7 +63,7 @@ def index():
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+    return render_template('base.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @app.route('/user/<username>')
