e5a2800538a0c82a8b913740d2db231269de30e9
xiaomacai <xmc1028@gmail.com>
2017-02-14 16:50:18 +0800

    session 8:add comfirm user email function

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,8 +3,9 @@ from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from .forms import LoginForm, RegistrationForm
 from ..models import User
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, current_user
 from .. import db
+from ..email import send_mail
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -33,9 +34,25 @@ def register():
         user = User(email=form.email.data, user_name=form.user_name.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        login_user(user)
+        token = user.generate_confirmation_token()
+        send_mail(user.email, u'账户邮箱确认', 'auth/mail/confirm', user=user, token=token)
+        flash(u'确认邮件已经发送到你的邮箱!')
         return redirect(url_for('main.index'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash(u'邮箱确认成功')
+    else:
+        flash(u'确认链接无效或过期')
+    return redirect(url_for('main.index'))
+
+
 
 
 
--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 
 class User(db.Model, UserMixin):
@@ -12,6 +14,23 @@ class User(db.Model, UserMixin):
     user_name = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.INTEGER, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.BOOLEAN, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     @property
     def password(self):
