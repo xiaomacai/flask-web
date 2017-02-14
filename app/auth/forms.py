987577d885fb8a70be6b1afd472150af4e8d1701
xiaomacai <xmc1028@gmail.com>
2017-02-14 15:50:39 +0800

    session 8: add register function

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,8 @@
 # coding: utf8
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from ..models import User
 
 
 class LoginForm(FlaskForm):
@@ -9,4 +10,20 @@ class LoginForm(FlaskForm):
     password = PasswordField(u'密码', validators=[DataRequired()])
     remember_me = BooleanField(u'保持登陆')
     submit = SubmitField(u'登陆')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField(u'邮箱', validators=[DataRequired(), Email(), Length(1, 64)])
+    user_name = StringField(u'用户名', validators=[DataRequired(), Length(1, 64)])
+    password = PasswordField(u'密码', validators=[DataRequired(), EqualTo('password2')])
+    password2 = PasswordField(u'确认密码', validators=[DataRequired()])
+    submit = SubmitField(u'注册')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(u'邮箱已经存在')
+
+    def validate_user_name(self, field):
+        if User.query.filter_by(user_name=field.data).first():
+            raise ValidationError(u'用户名已经存在')
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 # coding:utf8
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
+from .. import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -16,3 +17,25 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash(u'用户名或密码错误')
     return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, user_name=form.user_name.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', form=form)
+
+
+
