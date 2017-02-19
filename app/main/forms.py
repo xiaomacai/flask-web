8a264e80882560aaa8d950fe0cdcb3a659584449
xiaomacai <xmc1028@gmail.com>
2017-02-19 09:17:42 +0800

    session 10: finished user profile edit page

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,17 @@
 # coding: utf8
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class NameForm(FlaskForm):
     name = StringField(u'姓名', validators=[DataRequired()])
     submit = SubmitField(u'确定')
+
+
+class EditProfileForm(FlaskForm):
+    name = StringField(u'真实姓名', validators=[Length(0, 64)])
+    location = StringField(u'地址', validators=[Length(0, 64)])
+    about_me = TextAreaField(u'自我介绍')
+    submit = SubmitField(u'确定')
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, session, redirect, url_for, current_app, abort
 
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..models import User
+
+from flask_login import login_required, current_user
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -32,4 +34,21 @@ def user(user_name):
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
+
+
+@main.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        return redirect(url_for('main.user', user_name=current_user.user_name))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
 
